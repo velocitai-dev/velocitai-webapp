1714cd640b6f15e2be6db4cf61fd6639d832a061
adityamannu <adityamannu16@gmail.com>
2021-09-16 21:37:07 +0530

    first

---
+++ src/components/HeroSection.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <video src="/videos/video-1.mp4" autoPlay loop muted />
+      <h1>Your Technology Services Partner</h1>
+    </div>
+  );
+}
+
+export default HeroSection;
